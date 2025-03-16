4111cd89ea1039c170f9afa5979422359f2e1e6d
Siael Alves <siaelalves@outlook.com>
2025-03-16 15:23:14 -0300

    FEAT Criado inicializador para submódulo `data`;

---
+++ data.php
@@ -0,0 +1,20 @@
+<?php
+/* © Copyright 2024 - 2025, Siael Alves */
+
+/** Namespace para manipular e gerenciar os dados e os bancos de dados. */
+namespace data ;
+
+// Enum QUERY_DB
+require dirname(__FILE__) . "/QUERY_DB.php" ;
+
+// Enum QUERY_TYPE
+require dirname(__FILE__) . "/QUERY_TYPE.php" ;
+
+// Enum QUERY_ORDER
+require dirname(__FILE__) . "/QUERY_ORDER.php" ;
+
+// Classe database
+require dirname(__FILE__) . "/database.php" ;
+
+// Classe query
+require dirname(__FILE__) . "/query.php" ;
